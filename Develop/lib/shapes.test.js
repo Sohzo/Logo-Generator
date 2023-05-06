bd6437dc9fa89dee0f56519031820b00f272d00c
Jonathan Winter <jonpw2004@outlook.com>
2023-05-06 14:16:11 -0600

    Fixed Tests

--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -1,13 +1,30 @@
 const {Circle, Triangle, Square} = require("./shapes");
 
-const shape = new Triangle();
-shape.setTextColor("blue");
-expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
 
-const newCircle = new Circle();
-newCircle.setTextColor("blue");
-expect(newCircle.render()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue"></circle>');
 
-const newSquare = new Square();
-newSquare.setTextColor("blue");
-expect(newSquare.render()).toEqual('<rect x="50" height="200" width="200" fill="blue"></rect>');
+describe('Shape Tests', () => {
+
+    describe('Triangle', () => {
+        it("Should return a triangle SVG", () => {
+            const shape = new Triangle();
+            shape.setTextColor("blue");
+            expect(shape.render()).toEqual('<polygon height="100%" width="100%" points="150, 18 244, 182 56, 182" fill="blue"></polygon>');
+        });
+    });
+
+    describe('Circle', () => {
+        it("Should return a Circle SVG", () => {
+            const newCircle = new Circle();
+            newCircle.setTextColor("blue");
+            expect(newCircle.render()).toEqual('<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="blue"></circle>');
+        });
+    });
+
+    describe('Square', () => {
+        it("Should return a Square SVG", () => {
+            const newSquare = new Square();
+            newSquare.setTextColor("blue");
+            expect(newSquare.render()).toEqual('<rect x="50" height="200" width="200" fill="blue"></rect>');
+        });    
+    });
+});
